0bac26849e4fdc677d5d7ee72fd24890ba6da410
poberWong <poberwong@poberWongdeiMac.local>
2016-01-16 19:27:58 +0800

    HistoryList界面数据加载

---
+++ js.core/HistoryList.js
@@ -0,0 +1,148 @@
+'use strict'
+import React from 'react-native';
+var {
+  StyleSheet,
+  View,
+  ListView,
+  TouchableHighlight,
+  Image,
+  Text,
+  Component,
+} = React;
+
+/*
+date format: 2015/07/22
+date json array:
+	{
+"error": false,
+"results": [
+"2015-07-21",
+"2015-07-20",
+"2015-07-19",
+"2015-07-18",
+"2015-07-17",
+"2015-07-16",
+"2015-07-15",
+"2015-07-14",
+"2015-07-13",
+"2015-07-12"
+]
+}
+*/
+
+/*
+目标数据源： [{title: xxxx, thumbnail: xxx, date: xxx}...]
+date可以使用第一次请求的得来的数据
+*/
+
+var API_DATE_BEFORE = 'http://gank.avosapps.com/api/get/';
+var API_DAILY = 'http://gank.avosapps.com/api/day/';
+var PAGE_SIZE = 10;
+var LAST_DATE = '2016/01/15';
+var REQUEST_DATE_URL = API_DATE_BEFORE + PAGE_SIZE + '/before/' + LAST_DATE;
+
+class HistoryList extends Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  	  dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),//先初始化一个空的数据集合
+      loaded: false
+  	};
+  }
+
+
+  _convertDate(date: string){//change the date like '2015-11-05' into '2015/11/05'
+  	return date.replace('/\-/g', '/');//居然是一个一个替换,使用正则表达式解决方案
+  }
+
+  async componentDidMount(){
+  	var responseData = await this.fetchData(REQUEST_DATE_URL);//返回json对象
+  	console.log('responseData is '+ JSON.stringify(responseData));//JSON.stringify : converts a JavaScript value to a JSON string
+ 		for(var date of responseData.results){
+  		console.log(this._convertDate(date));
+  	}
+  }
+
+
+  async fetchData(url: string){
+   try{
+			let response = await fetch(url);
+			let responseData = await response.json();
+			//就是把一个javascript对象转换为JSON字符串
+			return responseData;
+    } catch (error) {
+    	console.log('data load failed :'+error);
+    }
+/*    fetch(url)
+    	.then((response) => response.json())
+    	.then((responseData) => {
+    		responseData;
+    		convert(responseData)
+    	};*/
+  }
+
+  render(){
+  	//在此处，使用整个加载试图在根布局上进行替换时，会造成ListView无法重新对顶部和底部的控件进行偏移
+  		return (
+  			<View style={styles.container}>
+  				<ListView
+  					dataSource={this.state.dataSource}
+  					renderRow={this._renderItem}
+  				/>
+  			</View>	
+  		);
+  }
+
+/*  _renderLoadingView(){
+  		return (
+  			<View style={[styles.container, {paddingTop: 20}]}>
+  				<Text>
+  					Loading......{'\n'}
+  					Loading......{'\n'}
+  					Loading......{'\n'}
+  					Loading......{'\n'}
+  					Loading......{'\n'}
+  				</Text>
+  			</View>
+  			);
+  }*/
+
+  _renderItem(date, sectionID, highlightRow){
+  	return (
+  		<TouchableHighlight>
+  		<View style={styles.itemContainer}>
+  			<Text>
+  			{date}
+  			</Text>
+  		</View>
+  		</TouchableHighlight>
+  	);
+  }
+
+}
+
+var styles = StyleSheet.create({
+	redbox: {
+		borderWidth: 2,
+		borderColor: '#FF0000',
+		flex: 1
+	},
+	container: {
+		flex: 1
+	},
+	itemContainer: {
+		flexDirection: 'row',
+		// height: 30,
+		backgroundColor: 'red',
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding : 10
+	},
+	title: {
+		fontSize: 26,
+		marginBottom: 6,
+	},
+
+});
+
+module.exports = HistoryList;
