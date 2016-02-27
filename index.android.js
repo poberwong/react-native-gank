de773b847f8c51e72b4d46210f860c82ad1676aa
poberWong <poberwong@gmail.com>
2016-02-27 20:57:28 +0800

    translucent statusBar with ReactNative instead of Native code

--- index.android.js
+++ index.android.js
@@ -12,6 +12,8 @@ var {
   StyleSheet,
   BackAndroid,
   Navigator,
+  View,
+  StatusBar,
   Component
 } = React
 
@@ -43,15 +45,20 @@ class ReactNativeGank extends Component {
   render () {
     // return (<HistoryList/>);
     return (
-      <Navigator style = {styles.container}
-        ref={component => this.navigator = component}
-        initialRoute={{
-          component: HomePage
-        }}
-        renderScene={(route, navigator) => { // 用来渲染navigator栈顶的route里的component页面
-          // route={component: xxx, name: xxx, ...}， navigator.......route 用来在对应界面获取其他键值
+      <View style = {styles.container}>
+        <StatusBar
+          backgroundColor='transparent'
+          translucent={true}/>
+        <Navigator
+          ref={component => this.navigator = component}
+          initialRoute={{
+            component: HomePage
+          }}
+          renderScene={(route, navigator) => { // 用来渲染navigator栈顶的route里的component页面
+            // route={component: xxx, name: xxx, ...}， navigator.......route 用来在对应界面获取其他键值
           return <route.component navigator={navigator} {...route} {...route.passProps}/>// {...route.passProps}即就是把passProps里的键值对全部以给属性赋值的方式展开 如：test={10}
         }}/>
+      </View>
     )
   }
 }
